f416638fa275cf8ea9fe722c88f652e3a549904d
sumanitian <sumanprasad26042001@gmail.com>
2021-11-02 10:34:24 +0530

    adminpage

---
+++ src/containers/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout'
+
+const Home = (props) => {
+    return(
+        <Layout>
+            <h1 style={{margin: '5rem',}} className="text-center" >Welcome To Admin Dashboard</h1>
+        </Layout>
+    )
+}
+
+export default Home
